9644dd2bc3de98dc78c8a079dcc21fcfc9eaee5c
johanahlin <johan.ahlin@lansforsakringar.se>
2019-12-16 10:45:56 +0100

    added placeholder image

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -8,13 +8,20 @@ import style from './index.module.css';
 
 const Card = ({title, url, image, description}) => {
   
-  let placeholder = '/img/placeholder.svg';
+  
    
   return (
     <Link className={style.Card} to={url} >
       <article className={style.Card__wrapper}>
         <div className={style.Card__image}>
-          <Image imageInfo={ image ? {image: image} : {image: placeholder}} />
+          {
+            image ?
+              <Image imageInfo={ {image: image}} />
+            :
+            <svg width="240" height="120" viewBox="0 0 240 120" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path stroke-width="2" stroke="#e5e5e5" fill="#ffffff" d="M0 0h239.625v119.927H0z"/><path fill="#DCDDDE" d="M18.433 41.231h202.759v3.574H18.433zM18.433 51.954h202.759v3.574H18.433zM18.433 62.677h202.759v3.574H18.433zM18.433 73.4h55.298v3.574H18.433z"/><text font-family="Arial-BoldMT, Arial" font-size="40" font-weight="bold" fill="#222"><tspan x="155.912" y="65.362">?</tspan></text></g></svg>
+
+          }
+          
         </div>
         <div className={style.Card__body}> 
           <h2 className={style.Card__title}>{title}</h2>
