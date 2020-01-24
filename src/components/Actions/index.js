27e691e720be41a2bd41779aeecfa7f123eb892a
johanahlin <johan.ahlin@lansforsakringar.se>
2020-01-24 08:30:41 +0100

    added code highlight

--- src/components/Actions/index.js
+++ src/components/Actions/index.js
@@ -22,11 +22,11 @@ const Actions = ({grey,white, toggleCode}) => {
               <svg className={style.Actions__icon} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="26" height="16"><defs><path id="a" d="M5.5 6.225l-6.293 6.293-1.414-1.414L5.5 3.396l7.707 7.708-1.414 1.414z"/></defs><defs><use xlinkHref="#a"/><use id="b" xlinkHref="#a"/></defs><g fill="none" fillRule="evenodd"><path d="M-11-8h32v32h-32z"/><use fill="#515151" transform="rotate(-90 5 8.457)" xlinkHref="#a"/><g><path d="M5-8h32v32H5z"/><use fill="#515151" transform="matrix(0 -1 -1 0 29.457 13.457)" xlinkHref="#b"/></g></g></svg>
           </button>
         </li>
-        <li className={style.Actions__item}>
+        {/* <li className={style.Actions__item}>
           <button className={style.Actions__button}>
             <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><defs><path d="M12 4v2H4v14h14v-8h2v10H2V4h10zm10-2v7h-2V5.414l-7.293 7.293-1.414-1.414L18.584 4H15V2h7z" id="d"/></defs><g transform="translate(-2 -2)" fill="none" fillRule="evenodd"><path d="M0 0h24v24H0z"/><use fill="#515151" xlinkHref="#d"/></g></svg>
           </button>
-        </li>
+        </li> */}
       </ul>
     </div>
   )
--- src/components/CodeBlock/index.js
+++ src/components/CodeBlock/index.js
@@ -1,36 +1,16 @@
 import React, {useRef, useEffect} from 'react';
 import style from './index.module.css';
-import Prism from "prismjs"
-
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const CodeBlock = ({code}) => {
-  const ref = useRef(null);
-  console.log(code)
-  useEffect(() => {
-    highlight()
-    console.log("Mount");
-  }, []);
 
-  useEffect(() => {
-    highlight()
-    console.log("Behavior when the component receives new state or props.", ref.current);
-    return function cleanup() {
-      highlight()
-      console.log("unmount.", ref.current);
-    };
-    
-  });
-
-  const highlight = () => {
-    if (ref.current) {
-      Prism.highlightElement(ref.current)
-    }
-  }
   return (
-    <pre className={style.CodeBlock}>
-      <code ref={ref} className={style.CodeBlock__code, 'language-html'}> {code.trim()}</code>
-    </pre>
-
+    <div className={style.CodeBlock}>
+      <SyntaxHighlighter language="javascript" style={tomorrow}>
+        {code}
+      </SyntaxHighlighter>
+    </div>
   );
 }
 
