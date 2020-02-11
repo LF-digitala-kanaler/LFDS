7d3a1c3a67b109930fbc54f3b1812025eea48d93
johanahlin <johan.ahlin@lansforsakringar.se>
2020-02-11 14:17:46 +0100

    added search animation

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react'
+import React, {  useState, useRef} from 'react'
 import { Link, navigate } from 'gatsby'
 import style from './index.module.css';
 import cx from 'classnames'
@@ -6,33 +6,36 @@ import { ArrowNavigation, useArrowNavigation } from 'react-arrow-navigation'
 
 // Search component
 
+const UseFocus = () => {
+	const htmlElRef = useRef(null)
+	const setFocus = () => {htmlElRef.current &&  htmlElRef.current.focus()}
 
+	return [ htmlElRef,  setFocus ] 
+}
 const Search = () => {
   const [query, setQuery] = useState('');
   const [isSearchVisible, setIsSearchVisible] = useState(false);  
   const [results, setResults] = useState([]);
-  
+  const [inputRef, setInputFocus] = UseFocus()
   const handleClick = () => {
     setIsSearchVisible(!isSearchVisible)
+    setInputFocus()
   }
   const SearchButton = () => {
       return (
         <button className={cx(style.Search__button, (isSearchVisible ? style['Search__button--isActive'] : '' ))} onClick={handleClick}>
-          <svg className={style.Search__icon} width="20" height="20" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path d="M-2-2h24v24H-2z"/><path d="M15.207 13.793l4.5 4.5-1.414 1.414-4.5-4.5 1.414-1.414zM8 0a8 8 0 110 16A8 8 0 018 0zm0 2a6 6 0 100 12A6 6 0 008 2z" fill="#515151"/></g></svg>
-          <span className={style.Search__text}>Sök</span>
+          <span className={style.Search__text}>Search</span>
+          <svg className={style.Search__icon} width="20" height="20" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path d="M-2-2h24v24H-2z"/><path d="M15.207 13.793l4.5 4.5-1.414 1.414-4.5-4.5 1.414-1.414zM8 0a8 8 0 110 16A8 8 0 018 0zm0 2a6 6 0 100 12A6 6 0 008 2z" fill="#515151"/></g></svg> 
         </button>
       )
   }
-  const ListItem = ({title, url, xIndex, yIndex, closeMenu}) => {
+  const ListItem = ({title, url, xIndex, yIndex}) => {
     const { selected, active } = useArrowNavigation(xIndex, yIndex)
    
     return (
       <Link 
         to={url} 
         className={cx(style.Search__link, selected && active ? style['Search__link--focused'] : '')}
-        onClick={() => {
-          closeMenu()
-        }}
       >
         {title}
       </Link>
@@ -44,7 +47,6 @@ const Search = () => {
       return(
        
         results.map((page, i) => (
-          
           <div className={style.Search__item} key={i}>
             <ListItem 
               url={page.url} 
@@ -52,8 +54,6 @@ const Search = () => {
               index={i} 
               xIndex={0} 
               yIndex={i}  
-              closeMenu={() => {console.log('hej')}}
-              
             />
           </div>
         ))
@@ -106,18 +106,20 @@ const Search = () => {
       <div className={style.Search}>
          <ArrowNavigation >
           <input
-            className={style.Search__field}
+            className={cx(style.Search__field, (isSearchVisible ? style['Search__field--isVisible'] : ''))}
             type="text"
             aria-label="Search"
             autoComplete="off"
             onChange={search}
-            placeholder={'Search'}
+           
+            ref={inputRef}
           />
         
           <div className={cx(style.Search__list, (results.length > 0 ? style['Search__list--isVisible'] : null))}>
             <ResultList />
           </div>
           </ArrowNavigation>
+        
          <SearchButton />
       </div>
     )
