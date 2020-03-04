25e32c24ddf7376281f3d5d8b86dfae551b06572
johanahlin <johan.ahlin@lansforsakringar.se>
2020-03-04 09:12:14 +0100

    updated lfui-components

--- src/components/CodeBlock/index.js
+++ src/components/CodeBlock/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import style from './index.module.css';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const CodeBlock = ({code}) => {
 
   return (
     <div className={style.CodeBlock}>
-      <SyntaxHighlighter language="javascript" style={tomorrow}>
+      <SyntaxHighlighter language="html" style={a11yDark}>
         {code}
       </SyntaxHighlighter>
     </div>
