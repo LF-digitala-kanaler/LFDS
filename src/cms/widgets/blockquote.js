5b922de0a7f20111eda55d74401a01fec97ca452
johanahlin <johan.ahlin@lansforsakringar.se>
2020-04-12 11:48:42 +0200

    changed blockq

--- src/cms/widgets/blockquote.js
+++ src/cms/widgets/blockquote.js
@@ -1,24 +1,32 @@
-
+import React from 'react';
+import Blockquote from "../../components/Blockquote"
 
 const Quote = {
   id: "blockquote",
   label: "Blockquote",
   fields: [
-    { name: "quote", label: "Qoute", widget: "string" },
-    { name: "cite", label: "Cite", widget: "string", required: false},
+    { name: "quote", label: "Qoute", widget: "string", default: ' ' },
+    { name: "cite", label: "Cite", widget: "string", required: false, default: ' '},
   ],
-  pattern: /^<blockquote class="Blockquote"><p>(.*)<\/p><footer class="Blockquote__author">(.*)<\/footer><\/blockquote>/,
+  pattern: /{"widget":"blockquote","quote":"(.*)","cite":"(.*)?"}/,
   fromBlock(match) {
+    const quote = match[1] ? match[1].replace(/^\s+/,'') : null
+    const cite = match[2] ? match[2].replace(/^\s+/,'') : null
     return {
-      quote: match[1],
-      cite: match[2],
+      quote,
+      cite,
     }
   },
   toBlock(obj) {
-    return '<blockquote class="Blockquote"><p>'+obj.quote+'</p><footer class="Blockquote__author">'+obj.cite+'</footer></blockquote>';
+    const data = JSON.stringify({
+			widget: 'blockquote',
+			quote: obj.quote,
+			cite: obj.cite,
+    });
+    return data
   },
   toPreview: function (obj) {
-    return '<blockquote class="Blockquote"><p>'+obj.quote+'</p><footer class="Blockquote__author">'+obj.cite+'</footer></blockquote>';
+    return (<Blockquote text={obj.quote} author={obj.cite} />)
   }
 }
 
