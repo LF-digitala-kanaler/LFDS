c5f2a87e7b69a56516ba2551b4b76cb54b256dd2
johanahlin <johan.ahlin@lansforsakringar.se>
2020-04-22 15:06:37 +0200

    button block

--- src/cms/widgets/buttonsBlock.js
+++ src/cms/widgets/buttonsBlock.js
@@ -2,27 +2,30 @@ import Immutable from 'immutable';
 const ButtonsBlock = {
   id: "buttonsBlock",
   label: "Buttons Block",
-  pattern: /<article><div class='ButtonContent'>(.*)<\/div>[^]*?<\/article>/,
+  pattern: /<article>[^]*?<\/article>/,
   fields: [{
-   
-    label: "Button",
-    name: "button",
-    widget: "list",
-    fields: [{
-        label: "Button text",
-        name: "text",
-        widget: "string",
-      },
-      {
-        label: "Button link",
-        name: "href",
-        widget: "string",
+      label: "Button Block", name: "buttons", widget: "object",
+      fields: [
+        {label: "Text", name: "content", widget: "markdown", editorComponents:['callout', 'advancedImage', 'code', 'code-block', 'color']},
+        {
+        label: "Button",
+        name: "button",
+        widget: "list",
+        fields: [{
+          label: "Button text",
+          name: "text",
+          widget: "string",
+        },
+        {
+          label: "Button link",
+          name: "href",
+          widget: "string",
+        }],
+      }], 
     }],
-     label: "Text", name: "content", widget: "markdown", editorComponents:['callout', 'advancedImage', 'code', 'code-block', 'color'],
-  }],
     
     fromBlock: function(match) {
-      console.log(match[1], match[2])
+      console.log(match)
       let matches = match[0].substring(match[0].indexOf("\n") + 1);
       matches = matches.substring(matches.lastIndexOf("\n") + 1, -1 )
       const buttonArray = matches.split(/(?=<Button)/);
@@ -31,8 +34,8 @@ const ButtonsBlock = {
       const items = buttonArray.map(function(item, index) {
         // console.log(item, 'items')
         return {
-          text: item.match(/text="(.*?)"/)[2],
-          href: item.match(/href="(.*?)"/)[2],
+          text: item.match(/text="(.*?)"/)[1],
+          href: item.match(/href="(.*?)"/)[1],
           
         }
        
@@ -47,7 +50,7 @@ const ButtonsBlock = {
 
 
     toBlock: function(obj) {
-      // console.log(obj)
+      console.log(obj)
       const items = Immutable.fromJS(obj.button || []).map(function(item, index) {
         return `<Button href="${item.get("href")}" text="${item.get("text")}"  />`
       });
