c20fbc52b9074e7366803f046fe78dc053785dec
johanahlin <johan.ahlin@lansforsakringar.se>
2020-04-28 15:36:02 +0200

    added flasg

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -16,12 +16,13 @@ const Card = ({title, url, image, description, grid, lang}) => {
             image != null ?
               <img className={style.Card__object} src={image.publicURL} alt=" " />
             :
-            <svg className={style.Card__empty} viewBox="0 0 240 120" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path strokeWidth="2" stroke="#e5e5e5" fill="#ffffff" d="M0 0h239.625v119.927H0z"/><path fill="#DCDDDE" d="M18.433 41.231h202.759v3.574H18.433zM18.433 51.954h202.759v3.574H18.433zM18.433 62.677h202.759v3.574H18.433zM18.433 73.4h55.298v3.574H18.433z"/><text fontFamily="Arial-BoldMT, Arial" fontSize="40" fontWeight="bold" fill="#222"><tspan x="155.912" y="65.362">?</tspan></text></g></svg>
+            <svg className={style.Card__empty}  xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path strokeWidth="2" stroke="#e5e5e5" fill="#ffffff" d="M0 0h239.625v119.927H0z"/><path fill="#DCDDDE" d="M18.433 41.231h202.759v3.574H18.433zM18.433 51.954h202.759v3.574H18.433zM18.433 62.677h202.759v3.574H18.433zM18.433 73.4h55.298v3.574H18.433z"/><text fontFamily="Arial-BoldMT, Arial" fontSize="40" fontWeight="bold" fill="#222"><tspan x="155.912" y="65.362">?</tspan></text></g></svg>
           }
         </div>
         <div className={style.Card__body}> 
-          <h2 className={cx(style.Card__title, (grid ? style['Card__title--small'] : '' ))}>{title} {lang && <svg className={style.Card__flag} width="18" height="12" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><defs><path id="a" d="M0 0h17.711v12H0z"/></defs><g fill="none" fillRule="evenodd"><path d="M0 0h17.711v12H0z" fill="#006AA7" fill-rule="nonzero"/><mask id="b" fill="#fff"><use xlinkHref="#a"/></mask><path fill="#FECC00" fillRule="nonzero" mask="url(#b)" d="M4.428 5H0v2h4.428v5h2.214V7h11.069V5H6.642V0H4.428z"/></g></svg>}</h2>
-          
+          <h2 className={cx(style.Card__title, (grid ? style['Card__title--small'] : '' ))}>{title} 
+          {lang && !grid && <svg className={style.Card__flag}  xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><defs><path id="a11" d="M0 0h33.621v25H0z"/></defs><g fill="none" fillRule="evenodd"><path d="M0 0h33.93v25H0z" fill="#006AA7" fillRule="nonzero"/><mask id="b11" fill="#fff"><use xlinkHref="#a11"/></mask><path fill="#FECC00" fillRule="nonzero" mask="url(#b11)" d="M8.405 10.417H0v4.166h8.405V25h4.203V14.583h21.013v-4.166H12.608V0H8.405z"/></g></svg>}
+          {lang && grid && <svg className={style.Card__flag} width="18" height="12" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><defs><path id="a" d="M0 0h17.711v12H0z"/></defs><g fill="none" fillRule="evenodd"><path d="M0 0h17.711v12H0z" fill="#006AA7" fillRule="nonzero"/><mask id="b" fill="#fff"><use xlinkHref="#a"/></mask><path fill="#FECC00" fillRule="nonzero" mask="url(#b)" d="M4.428 5H0v2h4.428v5h2.214V7h11.069V5H6.642V0H4.428z"/></g></svg>}</h2>
           {!grid &&<p className={style.Card__description}>{description}</p>}
         </div>
       </article>
