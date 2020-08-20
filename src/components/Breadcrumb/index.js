592f29b2383aaf9edd8db404d5e8f3ce1b3f2e0a
johan ahlin <johan.ahlin@lansforsakringar.se>
2020-08-20 15:21:38 +0200

    fixed breadcrumb

--- src/components/Breadcrumb/index.js
+++ src/components/Breadcrumb/index.js
@@ -1,20 +1,21 @@
 import React from "react"
 import style from './index.module.css';
 import { Link } from 'gatsby';
+import { stripTrailingSlash } from '../../utils/RemoveTrailingSlash';
 
 
 const parentDirectory = (url) => {
-
+  
   return url.substring(0, url.lastIndexOf('/'));
 }
 
 const Breadcrumb = ({location}) => {
-  console.log(location.location.pathname)
+  console.log(location.location.pathname.split('/'))
   return (
     <div className={style.Breadcrumb}>
       
         <li className={style.Breadcrumb__item}>
-          <Link className={style.Breadcrumb__link} to={location.location.pathname.includes('component')  ? location.location.pathname.split('/')[1]+'/'+location.location.pathname.split('/')[2] : location.location.pathname.split('/')[1]}>
+          <Link className={style.Breadcrumb__link} to={location.location.pathname.includes('component')  ? '/'+location.location.pathname.split('/')[1] : '/'+location.location.pathname.split('/')[1]}>
             {location.location.pathname.split('/')[1].replace('-', " ")}
           </Link>
         </li>
