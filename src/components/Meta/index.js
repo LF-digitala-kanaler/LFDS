879f72cc2bae26b0f6d38c803a175115dcc29f58
johan ahlin <johan.ahlin@lansforsakringar.se>
2020-09-07 20:30:11 +0200

    added twitter sharing

--- src/components/Meta/index.js
+++ src/components/Meta/index.js
@@ -29,24 +29,21 @@ export default class Meta extends Component {
     return (
       <Helmet>
         {console.log(description, 'desc')}
-        {title && <title>{title}</title>}
-        {pageTitle && <meta property="og:title" content={pageTitle} />}
+        
+        {title && <meta name="title" content={title} /> }
         {description && <meta name="description" content={description} />}
-        {description && (
-          <meta property="og:description" content={description} />
-        )}
+        
+        {pageTitle && <meta property="og:title" content={pageTitle} />}
+        {description && <meta property="og:description" content={description} />}
         {url && <meta property="og:type" content="website" />}
         {url && <meta property="og:url" content={url} />}
-        
-        <meta name="robots" content="noindex" />
-        {canonicalLink && <link rel="canonical" href={canonicalLink} />}
-
         <meta property="og:locale" content="en_US" />
         <meta property="og:site_name" content={pageTitle} />
-        
         <meta property="og:image:secure_url" content={absoluteImageUrl} />
         <meta property="og:image" content={absoluteImageUrl} />
-        <meta name="twitter:card" content={absoluteImageUrl} />
+
+        <meta name="robots" content="noindex" />
+        {canonicalLink && <link rel="canonical" href={canonicalLink} />}
         
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content={url} />
