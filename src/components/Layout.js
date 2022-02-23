ca1a57b4d430017463665e8bacc93eef4c28789c
Anton Trollbäck <anton@trollback.se>
2022-02-23 11:05:01 +0100

    Lint

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -52,9 +52,9 @@ const App = ({
               }}
             >
               {title}
-              <link rel="icon" href="/favicon.ico" sizes="any">
-              <link rel="icon" href="/icon.svg" type="image/svg+xml">
-              <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+              <link rel="icon" href="/favicon.ico" sizes="any" />
+              <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+              <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
             </Helmet>
 
             <Meta
